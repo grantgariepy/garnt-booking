8c468ceb829642b6c29096cb1a626e13722ce333
grantgariepy <grantgariepy@protonmail.com>
2022-05-29 14:03:21 -0600

    mail list

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import "./home.css"
 import Featured from "../../components/featured/Featured"
 import PropertyList from "../../components/propertyList/PropertyList"
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
+import MailList from "../../components/mailList/MailList"
 
 const Home = () => {
   return (
@@ -16,6 +17,7 @@ const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">More than just hotels... Bookers discover pure comfort with homes, apartments, and more</h1>
         <FeaturedProperties />
+        <MailList />
       </div>
     </div>
   )
