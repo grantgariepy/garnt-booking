e88dc7daf9f3c9efc6fd4a0d0c5ed03cdce177a4
grantgariepy <grantgariepy@protonmail.com>
2022-05-31 09:10:33 -0600

    fixed mobile search

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,29 +1,36 @@
+import useFetch from "../../hooks/useFetch";
 import "./featured.css"; 
 
 const Featured = () => {
+
+    const {data,loading,error} = useFetch("http://localhost:8800/api/hotels/countByCity?cities=berlin,madrid,london");
+    
+
   return (
     <div className="featured">
-        <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/540x270/957800.webp?k=6e50f4c36265ab07bf1c6498c298d8a9de40400f606365b83266a3e7ffe67877&o=" alt="" className="featuredImg" />
-            <div className="featuredTitles">
-                <h1>Dublin</h1>
-                <h2>23 Properties</h2>
+        { loading ? ("Loading Please Wait") : (  <>
+            <div className="featuredItem">
+                <img src="https://cf.bstatic.com/xdata/images/city/540x270/957800.webp?k=6e50f4c36265ab07bf1c6498c298d8a9de40400f606365b83266a3e7ffe67877&o=" alt="" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>Berlin</h1>
+                    <h2>{data[0]} Properties</h2>
+                </div>
             </div>
-        </div>
-        <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/540x270/690329.webp?k=330403cfa8e49ee98d01a4808f169d520e49f24348154b8cb7f637497015802f&o=" alt="" className="featuredImg" />
-            <div className="featuredTitles">
-                <h1>Austin</h1>
-                <h2>710 Properties</h2>
+            <div className="featuredItem">
+                <img src="https://cf.bstatic.com/xdata/images/city/540x270/690329.webp?k=330403cfa8e49ee98d01a4808f169d520e49f24348154b8cb7f637497015802f&o=" alt="" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>Madrid</h1>
+                    <h2>{data[1]} Properties</h2>
+                </div>
             </div>
-        </div>
-        <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/540x270/689874.webp?k=5a29b0d987e962b9ce2c78ae5dae52d02fb2ed959b1e2b00faf022306e103f8b&o=" alt="" className="featuredImg" />
-            <div className="featuredTitles">
-                <h1>Reno</h1>
-                <h2>87 Properties</h2>
+            <div className="featuredItem">
+                <img src="https://cf.bstatic.com/xdata/images/city/540x270/689874.webp?k=5a29b0d987e962b9ce2c78ae5dae52d02fb2ed959b1e2b00faf022306e103f8b&o=" alt="" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>London</h1>
+                    <h2>{data[2]} Properties</h2>
+                </div>
             </div>
-        </div>
+        </>)}
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import "./navbar.css"
+import {Link} from "react-router-dom"
 
 
 const Navbar = () => {
   return (
     <div className="navbar">
         <div className="navContainer">
-            <span className="logo" ><a href="/">grantbooking</a></span>
+          <Link to="/" style={{color: "inherit", textDecoration: "none", fontWeight:"bold"}}>
+            <span className="logo" >grantbooking</span>
+          </Link>  
             <div className="navItems">
                 <button className="navButton">Register</button>
                 <button className="navButton">Sign In</button>
